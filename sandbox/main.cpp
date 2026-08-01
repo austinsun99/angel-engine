ec96164fe62aacf7166edb3bd472e8d404aec3ac
austinsun99 <austinsun102@gmail.com>
2026-08-01 12:40:20 -0700

    chore: update build-all.bat to run program in same terminal feat: windows platform opening and closing a window

--- sandbox/main.cpp
+++ sandbox/main.cpp
@@ -1,11 +1,16 @@
 #include <core/platform/platform.h>
 
 int main() {
-    WindowConfig config = {.width = 1920, .height = 1080, .application_name = "PASTEL Engine"};
-    WindowState window_state = init_window(config);
-    open_window(&window_state);
+    WindowConfig config = {
+        .x                = 0,
+        .y                = 0,
+        .width            = 1920,
+        .height           = 1080,
+        .application_name = "PASTEL Engine",
+    };
 
-    while (pump_window(&window_state)) {
-    }
+    WindowState window_state = init_window();
+    open_window(config, &window_state);
+    while (pump_window(&window_state));
     deinit_window(&window_state);
 }
